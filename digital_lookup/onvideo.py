d13ef9e09e3d2159c1b6684be4f05bbf62f7ae8b
KudoS1410 <47254620+KudoS1410@users.noreply.github.com>
2019-12-09 18:42:38 +0530

    Reading the numbers in a video

---
+++ digital_lookup/onvideo.py
@@ -0,0 +1,111 @@
+import cv2 as cv
+import numpy as np
+from scipy import ndimage
+
+def read_digit(roi, DIGITS_LOOKUP):
+    (roiH, roiW) = roi.shape
+    (dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
+    dHC = int(roiH * 0.05)
+
+    w = roiW
+    h = roiH
+
+    segments = [
+        ((dW, 0), (w - dW, dH)),  # top
+        ((0, dH), (dW, (h // 2) - dHC)),  # top-left
+        ((w - dW, dH), (w, (h // 2) - dHC)),  # top-right
+        ((dW, (h // 2) - dHC), (w - dW, (h // 2) + dHC)),  # center
+        ((0, (h // 2) + dHC), (dW, h - dH)),  # bottom-left
+        ((w - dW, (h // 2) + dHC), (w, h - dH)),  # bottom-right
+        ((dW, h - dH), (w - dW, h))  # bottom
+    ]
+    on = [0] * len(segments)
+
+    for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
+        # extract the segment ROI, count the total number of
+        # thresholded pixels in the segment, and then compute
+        # the area of the segment
+        segROI = roi[yA:yB, xA:xB]
+        total = cv.countNonZero(segROI)
+        area = (xB - xA) * (yB - yA)
+
+        # if the total number of non-zero pixels is greater than
+        # 50% of the area, mark the segment as "on"
+        if total / float(area) <= 0.6:
+            # cv.rectangle(image, (xA, yA), (xB, yB), color=255, thickness=-1)
+            on[i] = 1
+    if tuple(on) in DIGITS_LOOKUP.keys():
+        digit = DIGITS_LOOKUP[tuple(on)]
+    else:
+        digit = 10
+    return(digit)
+
+def see_number(img):
+    DIGITS_LOOKUP = {
+        (1, 1, 1, 0, 1, 1, 1): 0,
+        (0, 0, 1, 0, 0, 1, 0): 1,
+        (1, 0, 1, 1, 1, 0, 1): 2,
+        (1, 0, 1, 1, 0, 1, 1): 3,
+        (0, 1, 1, 1, 0, 1, 0): 4,
+        (1, 1, 0, 1, 0, 1, 1): 5,
+        (1, 1, 0, 1, 1, 1, 1): 6,
+        (1, 0, 1, 0, 0, 1, 0): 7,
+        (1, 1, 1, 1, 1, 1, 1): 8,
+        (1, 1, 1, 1, 0, 1, 1): 9
+    }
+
+    image = cv.resize(img, (500, 500))
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    ret, thresh = cv.threshold(gray, 142.0, 255, type=cv.THRESH_BINARY)
+    ret, thresh = cv.threshold(thresh, 10, 255, type=cv.THRESH_BINARY)
+    thresh = cv.blur(thresh, (7, 7))
+    ret, thresh = cv.threshold(thresh, 250, 255, type=cv.THRESH_BINARY)
+    thresh = cv.erode(thresh, (5, 5), iterations=15)
+    roi = thresh[185:-70, 200:290].copy()
+    roi2 = thresh[185:-70, 295:375].copy()
+    roi2 = ndimage.rotate(roi2.copy(), 2.0, reshape=False)
+    roi2 = roi2[:, :-15]
+    roi3 = thresh[185:-70, 395:490].copy()
+    roi3 = ndimage.rotate(roi3.copy(), 3.0, reshape=False)
+    roi3 = roi3[:, :-15]
+    digit_img = [roi, roi2, roi3]
+    digits = []
+    for clip in digit_img:
+        d = read_digit(clip.copy(), DIGITS_LOOKUP)
+        if d < 10:
+            digits.append(d)
+        else:
+            digits = [10, 10, 10]
+            break
+    if digits[0] == 10:
+        return None
+    else:
+        return (digits[0]*10 + digits[1] + digits[2]*0.1)
+
+capture = cv.VideoCapture('vid1.webm')
+fps = int(capture.get(5))
+# print("fps: ", fps)
+
+count = 0
+while capture.isOpened():
+    ret, frame = capture.read()
+    # frame = cv.flip(frame, flipCode=1)
+    if ret:
+        img = frame.copy()
+        cv.imshow('camera', frame)
+        count = count + 1
+        # print(count)
+        if count > 0:
+            # cv.imwrite('img9.jpg', frame.copy())
+            print(see_number(frame.copy()))
+            # print(count)
+        k = cv.waitKey(1)
+
+        if k == ord('q'):
+            break
+        pass
+    else:
+        break
+    pass
+capture.release()
+cv.destroyAllWindows()
